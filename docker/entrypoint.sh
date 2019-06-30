cae7abc030f8cc7ef8247b1a9d201087b0a9cd37
Coo van Leeuwen <info@cerem.co>
2019-06-30 16:28:23 +0200

    Updated to add php host as variable

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,27 +5,38 @@ set -e
 BASE_HREF_DEFAULT=/lv2/
 BASE_HREF_PLACEHOLDER=/placeholder-base-href/
 BASE_HREF="${ANGULAR_BASE_HREF:-$BASE_HREF_DEFAULT}"
-# Escaped for regex
-BASE_HREF_PLACEHOLDER_ESC="$(echo "$BASE_HREF_PLACEHOLDER" | sed 's/[^-A-Za-z0-9_]/\\&/g')"
-BASE_HREF_ESC="$(echo "$BASE_HREF" | sed 's/[^-A-Za-z0-9_]/\\&/g')"
 
 # API URL STUFF #
 API_DEFAULT=api/
 API_PLACEHOLDER=lv2_api_url_placeholder
 API="${ANGULAR_API_URL:-$API_DEFAULT}"
-# Escaped for regex
-API_ESC="$(echo "$API" | sed 's/[^-A-Za-z0-9_]/\\&/g')"
+
+# Php FPM Upstream
+PHP_HOST_DEFAULT="php-upstream-name"
+PHP_HOST="${ANGULAR_PHP_HOST:-$PHP_HOST_DEFAULT}"
+
+# Function to replace text in files.
+replace() 
+{
+	# Takes 'filename', 'search_for', 'replace_with'
+	ESC2="$(echo "$2" | sed 's/[^-A-Za-z0-9_]/\\&/g')"
+	ESC3="$(echo "$3" | sed 's/[^-A-Za-z0-9_]/\\&/g')"
+	sed -i $1 -e "s/${ESC2}/${ESC3}/g"
+}
 
 echo Log Viewer 2 - Angular App
 echo Using base href: $BASE_HREF 
 echo API location: $API
+echo PHP FPM host: $PHP_HOST
 
 # Replace base href
-sed -i /app/lv2/index.html -e "s/${BASE_HREF_PLACEHOLDER_ESC}/${BASE_HREF_ESC}/g"
-sed -i /etc/nginx/conf.d/default.conf -e "s/${BASE_HREF_PLACEHOLDER_ESC}/${BASE_HREF_ESC}/g"
-
+replace /app/lv2/index.html $BASE_HREF_PLACEHOLDER $BASE_HREF
+replace /etc/nginx/conf.d/default.conf $BASE_HREF_PLACEHOLDER $BASE_HREF
+# Replace upstream
+replace /etc/nginx/conf.d/default.conf $PHP_HOST_DEFAULT $PHP_HOST
 # Replace API Url
-sed -i /app/lv2/main.*.js -e "s/${API_PLACEHOLDER}/${API_ESC}/g"
+replace /app/lv2/main.*.js $API_PLACEHOLDER $API
+
 
 # Move files to new base href location.
 if [ "$BASE_HREF" != "$BASE_HREF_DEFAULT" ]
